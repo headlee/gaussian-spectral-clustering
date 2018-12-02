da7368b5b7c5da16cf8a6b776f0efbbfbbc50e17
Jonathan Headlee <jonathan.headlee@gmail.com>
2018-12-02 16:29:53 -0500

    Add AVIRIS data loading function

--- ARCHER_data_handling.py
+++ ARCHER_data_handling.py
@@ -1,5 +1,6 @@
 import numpy as np
 import spectral.io.envi as envi # install with "pip install spectral"
+from spectral import open_image
 
 
 def load_archer_data(img_filename, img_truth_filename=None):
@@ -26,6 +27,35 @@ def load_archer_data(img_filename, img_truth_filename=None):
 
         # Read in image truth file
         img_truth = envi.open(img_truth_metadata, img_truth_filename)
+        truth_data = img_truth.load()
+
+        # Reshape truth data
+        truth_data_mat = truth_data.reshape([nl*ns, 1], order='F').transpose()
+
+    return hsi_data_mat, (nl, ns, nb), truth_data_mat
+
+
+def load_aviris_data(img_filename, img_truth_filename=None):
+    """
+    Loads a given AVIRIS image
+    :param img_filename: Full path and filename to AVIRIS HSI data on disk
+    :param img_truth_filename: (Optional) Full path and filename to ARCHER truth image on disk
+    :return: Tuple containing the HSI data mat, size of the data, and optionally the truth data mat
+    """
+    # Use third party spectral library to read in AVIRIS data
+    img = open_image(img_filename)
+
+    # Get data into a ImageArray, basically a numpy ndarray with some extra features
+    hsi_data = img.load()
+
+    nl, ns, nb = hsi_data.shape
+    hsi_data_mat = hsi_data.reshape([nl * ns, nb], order='F').transpose()
+
+    truth_data_mat = None
+
+    if img_truth_filename is not None:
+        # Read in image truth file
+        img_truth = open_image(img_truth_filename)
         truth_data = img_truth.load()
 
         # Reshape truth data
